10e5d949b7e63d9114122a3d4260cf91b4ac750c
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-21 17:46:50 -0500

    Add documentation for Person class and constructor

--- 2. Birthday Problem/src/Person.java
+++ 2. Birthday Problem/src/Person.java
@@ -1,7 +1,17 @@
+/**
+ * This class builds a Person object with a birthdya field, used to represent a student in the Class class.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Person
 {
     int birthday;
 
+    /**
+     * Constructs a Person object with the birthday set to the given parameter (in this case, a random day of the year).
+     * @param setBirthday   The birthday of the person.
+     */
     public Person(int setBirthday)
     {
         birthday = setBirthday;
