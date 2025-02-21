14bfa28223ce11111673d63d7921f8aa410dc743
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-21 17:53:35 -0500

    Add documentation

--- 2. Birthday Problem/src/Class.java
+++ 2. Birthday Problem/src/Class.java
@@ -1,12 +1,27 @@
+/**
+ * This class builds a Class object, used to represent a room of people (class of students) in the birthday problem. It
+ * contains methods to set random birthdays for all students, count the number of shared birthdays, and print the shared
+ * birthdays.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Class
 {
     int[] people;
 
+    /**
+     * Constructs a Class object with the given size of students.
+     * @param userSize  The size of the class.
+     */
     public Class(int userSize)
     {
         people = new int[userSize];
     }
 
+    /**
+     * Sets random birthdays for all students in the class.
+     */
     public void setRandomBirthdays()
     {
         for (int i = 0; i < people.length; i++)
@@ -15,6 +30,10 @@ public class Class
         }
     }
 
+    /**
+     * Counts the number of shared birthdays in the class.
+     * @return  The number of shared birthdays.
+     */
     public int countSameBirthday()
     {
         int counter = 0;
@@ -29,6 +48,10 @@ public class Class
         return counter;
     }
 
+    /**
+     * Prints the shared birthdays in the class.
+     * @return  The number of shared birthdays.
+     */
     public int printSharedBirthdays()
     {
         int counter = 0;
