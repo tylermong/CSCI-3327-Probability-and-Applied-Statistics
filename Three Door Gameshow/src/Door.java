8e5b8ba619e64693a57b5b102e612e128b27d825
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-21 17:24:23 -0500

    Enhance documentation

--- Three Door Gameshow/src/Door.java
+++ Three Door Gameshow/src/Door.java
@@ -1,12 +1,24 @@
+/**
+ * This class builds a door object with a contents field, used to represent a door in the Monty Hall problem.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Door
 {
     public String contents;
 
+    // TODO: Remove this constructor
     public Door()
     {
         contents = "placeholder";
     }
 
+    /**
+     * Constructs a door object with the contents set to the input.
+     * 
+     * @param userContents  The contents of the door.
+     */
     public Door(String userContents)
     {
         contents = userContents;
