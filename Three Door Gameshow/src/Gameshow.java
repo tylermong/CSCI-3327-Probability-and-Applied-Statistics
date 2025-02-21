734089cfaca9f61a314ec09d06362cd2399cded6
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-21 17:09:06 -0500

    Add documentation to outline logic clarity

--- Three Door Gameshow/src/Gameshow.java
+++ Three Door Gameshow/src/Gameshow.java
@@ -1,22 +1,52 @@
+/**
+ * Monty Hall problem simulation: Given three doors, one with a car behind it and the other two with goats, the player
+ * picks the initial door. Then the host opens one of the goat doors, leaving the player with two remaining doors. Now
+ * the player has the option to either stick with their initial choice, or switch to the other door.
+ * 
+ * This class simulates the game show for N rounds and returns the number of wins based on the player's choice.
+ *
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Gameshow
 {
+    /**
+     * Simulates the player sticking with their initial choice for N rounds.
+     * 
+     * @param numberOfRounds    the number of rounds to simulate
+     * @return                  the number of wins
+     */
     public int playGameStickWithChoice(int numberOfRounds)
     {
         return playGame(numberOfRounds, false);
     }
 
+    /**
+     * Simulates the player switching from their initial choice for N rounds.
+     * 
+     * @param numberOfRounds    the number of rounds to simulate
+     * @return                  the number of wins
+     */
     public int playGameSwitchChoice(int numberOfRounds)
     {
         return playGame(numberOfRounds, true);
     }
 
+    /**
+     * Simulates playing the game for N rounds.
+     * 
+     * @param numberOfRounds    the number of rounds to simulate
+     * @param switchChoice      player's decision to switch doors
+     * @return                  the number of wins
+     */
     private int playGame(int numberOfRounds, boolean switchChoice)
     {
         int winCounter = 0;
 
+        // Play the game for N rounds.
         for (int roundNumber = 0; roundNumber < numberOfRounds; roundNumber++)
         {
-            // set up the doors -- put goats behind them all
+            // Create the three doors and put a goat behind each.
             Door[] doors = new Door[3];
             for (int i = 0; i < doors.length; i++)
             {
@@ -24,49 +54,68 @@ public class Gameshow
                 doors[i].contents = "goat";
             }
 
-            // then pick the winning door and put a goat behind it
+            // Pick the winning door and put a goat behind it.
             int winningDoorNumber = (int) (Math.random() * 3);
             doors[winningDoorNumber].contents = "car";
 
-            // have the "player" select a random door
+            // Have the "player" select a random door.
             int selectedDoorNumber = (int) (Math.random() * 3);
 
-            // eliminate a door
+            // Eliminate a goat door.
             int eliminatedDoorNumber = eliminateGoatDoor(doors, selectedDoorNumber, winningDoorNumber);
 
-            // after eliminating a door, if they want to switch, then switch
+            // After eliminating a door, if they want to switch, then switch doors.
             if (switchChoice)
             {
                 selectedDoorNumber = switchDoor(doors, selectedDoorNumber, eliminatedDoorNumber);
             }
 
-            // now check if they selected the winning door
+            // Finally check if they selected the winning door. If so, increment the win counter.
             if (doors[selectedDoorNumber].contents.equals("car"))
             {
                 winCounter++;
             }
         }
-
         return winCounter;
     }
 
+    /**
+     * Eliminates a goat door that the player did not select.
+     * 
+     * @param doors                 the three doors
+     * @param selectedDoorNumber    the door the player selected
+     * @param winningDoorNumber     the door with the car behind it
+     * @return                      the eliminated door number
+     */
     private int eliminateGoatDoor(Door[] doors, int selectedDoorNumber, int winningDoorNumber)
     {
         for (int i = 0; i < doors.length; i++)
         {
+            // If the current door is not the selected door and is not the winning door, then eliminate it, by changing
+            // its contents from "goat" to "removed".
             if (i != selectedDoorNumber && doors[i].contents.equals("goat"))
             {
                 doors[i].contents = "removed";
                 return i;
             }
         }
+        // If no goat doors were found, return -1. (This should never happen.)
         return -1;
     }
 
+    /**
+     * Switches the player's selected door (if they want to switch)
+     * 
+     * @param doors                 the three doors (two remaining + one eliminated)
+     * @param selectedDoorNumber    the door the player selected
+     * @param eliminatedDoorNumber  the door that was eliminated
+     * @return                      the switched door number
+     */
     private int switchDoor(Door[] doors, int selectedDoorNumber, int eliminatedDoorNumber)
     {
         for (int i = 0; i < doors.length; i++)
         {
+            // If the current door is not the selected door and is not the eliminated door, then switch to it.
             if (i != selectedDoorNumber && i != eliminatedDoorNumber)
             {
                 return i;
