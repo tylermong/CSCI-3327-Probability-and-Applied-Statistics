08e0107a996473285d4acf899127d664e252e104
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-25 20:27:14 -0500

    Add GrassEnergy class extending EnergyCard with appropriate attributes

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/GrassEnergy.java
@@ -0,0 +1,11 @@
+package card.energy;
+
+import card.EnergyCard;
+
+public class GrassEnergy extends EnergyCard
+{
+    public GrassEnergy()
+    {
+        super("Grass Energy", "Grass");
+    }
+}
