59024c3f28b6d8a5ea6daf55f4cf1a504040915e
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-25 20:29:43 -0500

    Add DoubleColorlessEnergy class extending EnergyCard with appropriate attributes

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/DoubleColorlessEnergy.java
@@ -0,0 +1,11 @@
+package card.energy;
+
+import card.EnergyCard;
+
+public class DoubleColorlessEnergy extends EnergyCard
+{
+    public DoubleColorlessEnergy()
+    {
+        super("Double Colorless Energy", "Colorless");
+    }
+}
