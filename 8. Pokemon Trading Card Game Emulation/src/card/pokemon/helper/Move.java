b7f77b02763104f3d0605966ff45f7b213fcfb58
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-25 20:35:46 -0500

    Add energyCost attribute to Move class and update constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/pokemon/helper/Move.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/helper/Move.java
@@ -1,15 +1,18 @@
 package card.pokemon.helper;
 
+import java.util.Map;
 public class Move
 {
     private String name;
     private int damage;
+    private Map<String, Integer> energyCost;
     private String effect;
 
-    public Move(String name, int damage, String effect)
+    public Move(String name, int damage, Map<String, Integer> energyCost, String effect)
     {
         this.name = name;
         this.damage = damage;
+        this.energyCost = energyCost;
         this.effect = effect;
     }
 
@@ -21,6 +24,11 @@ public class Move
     public int getDamage()
     {
         return damage;
+    }
+
+    public Map<String, Integer> getEnergyCost()
+    {
+        return energyCost;
     }
 
     public String getEffect()
