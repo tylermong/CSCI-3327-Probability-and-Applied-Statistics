75573a50c475cd188fa7680743aa6395abf9aa7f
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-25 20:24:10 -0500

    Add EnergyCard class extending Card with energy type attribute

--- 8. Pokemon Trading Card Game Emulation/src/card/EnergyCard.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/EnergyCard.java
@@ -1,6 +1,17 @@
 package card;
 
-public class EnergyCard
+public class EnergyCard extends Card
 {
-    
+    private String energyType;
+
+    public EnergyCard(String name, String energyType)
+    {
+        super(name);
+        this.energyType = energyType;
+    }
+
+    public String getType()
+    {
+        return energyType;
+    }
 }
