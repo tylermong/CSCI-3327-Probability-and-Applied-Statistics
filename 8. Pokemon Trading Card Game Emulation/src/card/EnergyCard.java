f6dbad668172c731408e17721c199f1ebd63a758
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-25 20:31:00 -0500

    Enhance EnergyCard to include energyValue attribute and update constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/EnergyCard.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/EnergyCard.java
@@ -3,15 +3,22 @@ package card;
 public class EnergyCard extends Card
 {
     private String energyType;
+    private int energyValue;
 
-    public EnergyCard(String name, String energyType)
+    public EnergyCard(String name, String energyType, int energyValue)
     {
         super(name);
         this.energyType = energyType;
+        this.energyValue = energyValue;
     }
 
     public String getType()
     {
         return energyType;
+    }
+
+    public int energyValue()
+    {
+        return energyValue;
     }
 }
