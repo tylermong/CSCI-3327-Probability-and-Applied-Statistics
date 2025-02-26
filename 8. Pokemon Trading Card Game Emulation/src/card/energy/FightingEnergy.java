3aae4193c01a5544c37228315c2d5c00c0f25d5c
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-25 20:26:09 -0500

    Add FightingEnergy class extending EnergyCard with appropriate attributes

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/FightingEnergy.java
@@ -0,0 +1,11 @@
+package card.energy;
+
+import card.EnergyCard;
+
+public class FightingEnergy extends EnergyCard
+{
+    public FightingEnergy()
+    {
+        super("Fighting Energy", "Fighting");
+    }
+}
