91296cbeb47932646361447b998e8d2a5b174d69
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-26 09:04:10 -0500

    Refactor TrainerCard to be abstract and add description attribute with getter

--- 8. Pokemon Trading Card Game Emulation/src/card/TrainerCard.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/TrainerCard.java
@@ -1,6 +1,19 @@
 package card;
 
-public class TrainerCard
+public abstract class TrainerCard extends Card
 {
-    
+    private String description;
+
+    public TrainerCard(String name, String description)
+    {
+        super(name);
+        this.description = description;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public abstract void useEffect();
 }
