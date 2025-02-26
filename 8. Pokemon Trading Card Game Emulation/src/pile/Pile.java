50c4a5380ed845ac7cbd005e5bb3b2136cac8cb5
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-26 10:50:13 -0500

    Add abstract Pile class for managing card collections

---
+++ 8. Pokemon Trading Card Game Emulation/src/pile/Pile.java
@@ -0,0 +1,25 @@
+package pile;
+
+import card.Card;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Pile
+{
+    private List<Card> cards;
+
+    public Pile()
+    {
+        this.cards = new ArrayList<>();
+    }
+
+    public void addCard(Card card)
+    {
+        cards.add(card);
+    }
+
+    public void removeCard(Card card)
+    {
+        cards.remove(0);
+    }
+}
