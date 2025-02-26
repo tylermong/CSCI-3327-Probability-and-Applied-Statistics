bc3c793021577ca1d01da4c8ad1f631f6c1a09de
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-02-25 20:27:45 -0500

    Add PsychicEnergy class extending EnergyCard with appropriate attributes

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/PsychicEnergy.java
@@ -0,0 +1,11 @@
+package card.energy;
+
+import card.EnergyCard;
+
+public class PsychicEnergy extends EnergyCard
+{
+    public PsychicEnergy()
+    {
+        super("Psychic Energy", "Psychic");
+    }
+}
