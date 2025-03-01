503ecc987b0bc6ca832245aa7d9dcc8bd7fa068b
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-01 15:35:50 -0500

    Enhance deck selection process with validation and improved formatting

--- 8. Pokemon Trading Card Game Emulation/src/game/Game.java
+++ 8. Pokemon Trading Card Game Emulation/src/game/Game.java
@@ -22,12 +22,24 @@ public class Game
         DeckBuilder.initializePrebuiltDecks();
         String[] decks = DeckBuilder.getAllPrebuiltDecks();
 
+        // Get deck selection from the player
         System.out.println("Select a deck for " + player.getName() + ": ");
         for (int i = 0; i < decks.length; i++)
         {
-            System.out.println((i + 1) + ". " + decks[i]);
+            System.out.println("\t" + (i + 1) + ". " + decks[i]);
         }
+        System.out.print(player.getName() + " selection: ");
         int deckChoice = in.nextInt() - 1;
+
+        // Validate the selection
+        while (deckChoice < 0 || deckChoice >= decks.length)
+        {
+            System.out.print("Invalid selection. Please choose again (1 - " + decks.length + "): ");
+            deckChoice = in.nextInt() - 1;
+        }
+        
+        // Set the selected deck for the player
+        System.out.println(player.getName() + " selected " + decks[deckChoice] + " deck.\n");
         player.setDeck(DeckBuilder.getPrebuiltDeck(decks[deckChoice]));
     }
 }
