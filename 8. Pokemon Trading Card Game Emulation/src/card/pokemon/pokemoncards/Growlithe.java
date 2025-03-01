3beb32fe13c68b3441ba982e39eb1151bbeb7c6f
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-01 12:59:44 -0500

    Add Growlithe Pokémon card

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Growlithe.java
@@ -0,0 +1,15 @@
+package card.pokemon.pokemoncards;
+
+import java.util.Map;
+
+import card.pokemon.PokemonCard;
+import card.pokemon.helper.Move;
+
+public class Growlithe extends PokemonCard
+{
+    public Growlithe()
+    {
+        super("Growlithe", "Water", null, 1);
+        addMove(new Move("Flare", 20, Map.of("Fire", 1, "Colorless", 1), "No additional effect."));
+    }
+}
