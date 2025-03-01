7d3b65d1f0f2433cd053a8dd981b09ea184146b7
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-01 15:08:46 -0500

    Refactor Game class to use Player objects and integrate deck selection process

--- 8. Pokemon Trading Card Game Emulation/src/game/Game.java
+++ 8. Pokemon Trading Card Game Emulation/src/game/Game.java
@@ -4,49 +4,25 @@ import pile.piles.*;
 
 public class Game
 {
-    private Deck player1Deck, player2Deck;
-    private Hand player1Hand, player2Hand;
-    private Prize player1Prize, player2Prize;
-    private DiscardPile player1DiscardPile, player2DiscardPile;
+    private Player player1, player2, currentPlayer;
 
     public Game()
     {
-        player1Deck = new Deck();
-        player2Deck = new Deck();
-
-        player1Hand = new Hand();
-        player2Hand = new Hand();
-
-        player1Prize = new Prize();
-        player2Prize = new Prize();
-
-        player1DiscardPile = new DiscardPile();
-        player2DiscardPile = new DiscardPile();
+        player1 = new Player("Player 1");
+        player2 = new Player("Player 2");
+        
+        selectDecks();
     }
 
-    public void startGame()
+    private void selectDecks()
     {
-        // Shuffles the decks
-        System.out.println("[System] Starting game...");
-        System.out.println("[System] Shuffling decks...");
-        player1Deck.shuffle();
-        player2Deck.shuffle();
-
-        // Draw initial hand
-        System.out.println("[System] Drawing initial hands...");
-        for (int i = 0; i < 7; i++)
+        DeckBuilder.initializePrebuiltDecks();
+        String[] decks = DeckBuilder.getAllPrebuiltDecks();
+        System.out.println("Select a deck for Player 1:");
+        for (int i = 0; i < decks.length; i++)
         {
-            player1Hand.addCard(player1Deck.drawCard());
-            player2Hand.addCard(player2Deck.drawCard());
+            System.out.println((i + 1) + ". " + decks[i]);
         }
 
-        // Place prize cards
-        System.out.println("[System] Placing prize cards...");
-        for (int i = 0; i < 6; i++)
-        {
-            player1Prize.addCard(player1Deck.drawCard());
-            player2Prize.addCard(player2Deck.drawCard());
-        }
-        System.out.println("[System] Game started successfully.");
     }
 }
