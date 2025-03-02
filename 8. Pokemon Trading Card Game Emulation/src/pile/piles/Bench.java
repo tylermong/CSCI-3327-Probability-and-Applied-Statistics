e8c819e8135ae6bf24b190a68f87329107a2ad87
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-02 17:42:52 -0500

    Add maximum size constant and getter method to Bench class

--- 8. Pokemon Trading Card Game Emulation/src/pile/piles/Bench.java
+++ 8. Pokemon Trading Card Game Emulation/src/pile/piles/Bench.java
@@ -4,8 +4,15 @@ import pile.Pile;
 
 public class Bench extends Pile
 {
+    private static final int MAX_SIZE = 5;
+
     public Bench()
     {
         super();
+    }
+
+    public int getMaxSize()
+    {
+        return MAX_SIZE;
     }
 }
