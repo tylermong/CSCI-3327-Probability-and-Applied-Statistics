0e464dba371007912f9f11dc4137c2aad3cea32f
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-01 19:11:26 -0500

    Reorder methods for consistent logic flow

--- 8. Pokemon Trading Card Game Emulation/src/game/Game.java
+++ 8. Pokemon Trading Card Game Emulation/src/game/Game.java
@@ -73,6 +73,32 @@ public class Game
         addPrizePokemon(player2);
     }
 
+    private void selectDeck(Player player)
+    {
+        DeckBuilder.initializePrebuiltDecks();
+        String[] decks = DeckBuilder.getAllPrebuiltDecks();
+
+        // Get deck selection from the player
+        System.out.println("Select a deck for " + player.getName() + ": ");
+        for (int i = 0; i < decks.length; i++)
+        {
+            System.out.println("\t" + (i + 1) + ". " + decks[i]);
+        }
+        System.out.print(player.getName() + " selection: ");
+        int deckChoice = in.nextInt() - 1;
+
+        // Validate the selection
+        while (deckChoice < 0 || deckChoice >= decks.length)
+        {
+            System.out.print("Invalid selection. Please choose again (1 - " + decks.length + "): ");
+            deckChoice = in.nextInt() - 1;
+        }
+        
+        // Set the selected deck for the player
+        System.out.println(player.getName() + " selected " + decks[deckChoice] + " deck.\n");
+        player.setDeck(DeckBuilder.getPrebuiltDeck(decks[deckChoice]));
+    }
+
     public String flipCoin()
     {
         if (Math.random() < 0.5)
@@ -173,31 +199,5 @@ public class Game
             Card prizeCard = player.getDeck().drawCard();
             player.getPrizeCards().addCard(prizeCard);
         }
-    }
-
-    private void selectDeck(Player player)
-    {
-        DeckBuilder.initializePrebuiltDecks();
-        String[] decks = DeckBuilder.getAllPrebuiltDecks();
-
-        // Get deck selection from the player
-        System.out.println("Select a deck for " + player.getName() + ": ");
-        for (int i = 0; i < decks.length; i++)
-        {
-            System.out.println("\t" + (i + 1) + ". " + decks[i]);
-        }
-        System.out.print(player.getName() + " selection: ");
-        int deckChoice = in.nextInt() - 1;
-
-        // Validate the selection
-        while (deckChoice < 0 || deckChoice >= decks.length)
-        {
-            System.out.print("Invalid selection. Please choose again (1 - " + decks.length + "): ");
-            deckChoice = in.nextInt() - 1;
-        }
-        
-        // Set the selected deck for the player
-        System.out.println(player.getName() + " selected " + decks[deckChoice] + " deck.\n");
-        player.setDeck(DeckBuilder.getPrebuiltDeck(decks[deckChoice]));
     }
 }
