d4ee3503de27e56dfc4aad59bbe03f199fb6be62
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-02 20:19:51 -0500

    Add energy cost validation to attack method in Active class

--- 8. Pokemon Trading Card Game Emulation/src/pile/piles/Active.java
+++ 8. Pokemon Trading Card Game Emulation/src/pile/piles/Active.java
@@ -6,6 +6,7 @@ import card.Card;
 import card.pokemon.PokemonCard;
 import card.pokemon.helper.Move;
 import pile.Pile;
+import java.util.Map;
 
 public class Active extends Pile
 {
@@ -15,7 +16,6 @@ public class Active extends Pile
         super();
     }
 
-    // TODO: check for energy requirement
     public boolean attack(Active target)
     {
         PokemonCard attacker = (PokemonCard) getCardAtIndex(0);
@@ -37,22 +37,38 @@ public class Active extends Pile
         System.out.println("\nMoves:");
         for (int i = 0; i < moves.length; i++)
         {
-            System.out.println("\t" + (i + 1) + ". " + moves[i].getName() + " - Damage: " + moves[i].getDamage() + " - " + moves[i].getEffect());
+            System.out.println("\t" + (i + 1) + ". " + moves[i].getName() + " - Damage: " + moves[i].getDamage() + " - " + moves[i].getEffect() + " - " + moves[i].getEnergyCost());
         }
         System.out.print("Selection: ");
         int selectedMoveIndex = in.nextInt() - 1;
 
+        // Validate within range
         while(selectedMoveIndex < 0 || selectedMoveIndex >= moves.length)
         {
             System.out.println("Invalid selection. Please choose a valid move (1 - " + moves.length + "): ");
             selectedMoveIndex = in.nextInt() - 1;
         }
+
+        // Validate energy cost
+        Map<String, Integer> energyCost = moves[selectedMoveIndex].getEnergyCost();
+        for (Map.Entry<String, Integer> entry : energyCost.entrySet())
+        {
+            String energyType = entry.getKey();
+            int requiredAmount = entry.getValue();
+            int availableAmount = attacker.getTypeEnergy(energyType);
+            if (availableAmount < requiredAmount)
+            {
+                System.out.println("Required energy is not available. Please select a different move or skip the attack.");
+                return selectMove(attacker, defender);
+            }
+        }
+        // At this point, the move is valid and the energy cost is met
         return moves[selectedMoveIndex];
     }
 
     private void applyMoveEffect(Move move, PokemonCard attacker, PokemonCard defender)
     {
-        // TODO: Implment effect logic
+        // TODO: Implement effect logic
     }
 
     private int calculateMoveDamage(Move move, PokemonCard attacker, PokemonCard defender)
