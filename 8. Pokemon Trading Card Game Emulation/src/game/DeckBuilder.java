1f770b0abf357e1573133dd2c979820a47017902
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-14 15:29:56 -0400

    Rename testing-specific prebuilt decks

--- 8. Pokemon Trading Card Game Emulation/src/game/DeckBuilder.java
+++ 8. Pokemon Trading Card Game Emulation/src/game/DeckBuilder.java
@@ -114,7 +114,7 @@ public class DeckBuilder
         for (int i = 0; i < 37; i++) mulliganDeck.addCard(new FightingEnergy());
 
         // Add all 60 cards to the deck
-        prebuiltDecks.put("Mulligan", mulliganDeck);
+        prebuiltDecks.put("[Testing] Mulligan", mulliganDeck);
     }
 
     private static void initializeEnergyDeck()
@@ -132,7 +132,7 @@ public class DeckBuilder
         for (int i = 0; i < 10; i++) energyDeck.addCard(new DoubleColorlessEnergy());
 
         // Add all 60 cards to the deck
-        prebuiltDecks.put("Energy", energyDeck);
+        prebuiltDecks.put("[Testing] Energy", energyDeck);
     }
 
     public static String[] getAllPrebuiltDecks()
