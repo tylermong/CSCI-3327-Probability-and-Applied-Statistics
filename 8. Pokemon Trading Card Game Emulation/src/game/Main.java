eab76b8cb8884832660bc35648b92e2cf41db544
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 12:52:41 -0400

    Add documentation comments to Main class and main method

--- 8. Pokemon Trading Card Game Emulation/src/game/Main.java
+++ 8. Pokemon Trading Card Game Emulation/src/game/Main.java
@@ -1,7 +1,17 @@
 package game;
 
+/**
+ * This class runs the Pokémon Trading Card Game emulation. It initializes the game and starts the game loop/
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Main
 {
+    /**
+     * Sets up the game and starts the game loop.
+     * @param args  Command line arguments.
+     */
     public static void main(String[] args)
     {
         Game game = new Game();
