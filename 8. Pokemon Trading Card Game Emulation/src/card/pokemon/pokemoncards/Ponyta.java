e60e3392d2578ae730a9d52c880e3d5f79b87f82
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:35:56 -0400

    Add documentation comments for Ponyta class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Ponyta.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Ponyta.java
@@ -5,8 +5,20 @@ import java.util.Map;
 import card.pokemon.PokemonCard;
 import card.pokemon.helper.Move;
 
+/**
+ * This class represents a "Ponyta" Pokémon card in the Pokémon Trading Card Game.
+ * It extends the PokemonCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * It also adds two unique moves to the card: "Smash Kick" and "Flame Tail".
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Ponyta extends PokemonCard
 {
+    /**
+     * Constructor to initialize a "Ponyta" Pokémon card with its unique attributes.
+     * It also adds two unique moves to the card: "Smash Kick" and "Flame Tail".
+     */
     public Ponyta()
     {
         super("Ponyta", 40, "Water", null, 1);
