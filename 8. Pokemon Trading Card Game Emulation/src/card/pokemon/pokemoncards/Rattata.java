6218bdffb32c4a64353402adb564671af8af1da2
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:35:59 -0400

    Add documentation comments for Rattata class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Rattata.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Rattata.java
@@ -5,8 +5,20 @@ import java.util.Map;
 import card.pokemon.PokemonCard;
 import card.pokemon.helper.Move;
 
+/**
+ * This class represents a "Rattata" Pokémon card in the Pokémon Trading Card Game.
+ * It extends the PokemonCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * It also adds a unique move to the card: "Pound".
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Rattata extends PokemonCard
 {
+    /**
+     * Constructor to initialize a "Rattata" Pokémon card with its unique attributes.
+     * It also adds a unique move to the card: "Bite".
+     */
     public Rattata()
     {
         super("Rattata",30, "Fighting", "Psychic", 0);
