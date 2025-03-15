fe6d23c26fe25b6cfbeca7ee4a83121317bc0b22
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 15:41:47 -0400

    Add documentation comments to Hand class and its methods

--- 8. Pokemon Trading Card Game Emulation/src/pile/piles/Hand.java
+++ 8. Pokemon Trading Card Game Emulation/src/pile/piles/Hand.java
@@ -3,30 +3,53 @@ package pile.piles;
 import card.Card;
 import pile.Pile;
 
+/**
+ * Represents the player's hand in the Pokémon Trading Card Game, where the player holds their cards.
+ * This class extends the Pile class and provides methods for managing the player's hand, such as playing a card and checking for basic Pokémon.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Hand extends Pile
 {
+    /**
+     * Constructor for the Hand class. Initializes the player's hand.
+     */
     public Hand()
     {
         super();
     }
 
+    /**
+     * Plays a card from the player's hand and removes it from the hand.
+     * 
+     * @param card  The card to be played.
+     * @return      The played card.
+     */
     public Card playCard(Card card)
     {
         removeCard(card);
         return card;
     }
 
+    /**
+     * Checks if the player's hand contains any basic Pokémon cards.
+     * This is used in the game setup to check for a Mulligan.
+     * @return  True if there are basic Pokémon cards, false otherwise.
+     */
     public boolean hasBasicPokemon()
     {
+        // For each card in the hand, check if it is a basic Pokémon card.
         for (Card card : cards)
         {
+            // If a basic Pokémon card is found, return true.
             if (card.getType().equals("Pokémon"))
             {
                 return true;
             }
         }
+        
+        // If no basic Pokémon cards are found, return false.
         return false;
     }
-
-
 }
