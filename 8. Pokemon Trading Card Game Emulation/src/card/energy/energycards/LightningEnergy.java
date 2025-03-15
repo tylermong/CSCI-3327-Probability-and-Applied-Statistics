199bd688433ffbbee9a31b6fa60b6e43c9d63352
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:50:03 -0400

    Add documentation comments for LightningEnergy class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/LightningEnergy.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/LightningEnergy.java
@@ -2,8 +2,18 @@ package card.energy.energycards;
 
 import card.energy.EnergyCard;
 
+/**
+ * This class represents a "Lightning Energy" card in the Pokémon Trading Card Game.
+ * It extends the EnergyCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class LightningEnergy extends EnergyCard
 {
+    /**
+     * Constructor to initialize a "Lightning Energy" card with its type and value.
+     */
     public LightningEnergy()
     {
         super("Lightning Energy", "Lightning", 1);
