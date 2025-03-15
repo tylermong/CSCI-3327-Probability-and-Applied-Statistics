491d0a189fe7184bd161d3d5c6c81c32caa8275a
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:50:16 -0400

    Add documentation comments for DoubleColorlessEnergy class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/DoubleColorlessEnergy.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/DoubleColorlessEnergy.java
@@ -2,8 +2,18 @@ package card.energy.energycards;
 
 import card.energy.EnergyCard;
 
+/**
+ * This class represents a "Double Colorless Energy" card in the Pokémon Trading Card Game.
+ * It extends the EnergyCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class DoubleColorlessEnergy extends EnergyCard
 {
+    /**
+     * Constructor to initialize a "Double Colorless Energy" card with its type and value.
+     */
     public DoubleColorlessEnergy()
     {
         super("Double Colorless Energy", "Colorless", 2);
