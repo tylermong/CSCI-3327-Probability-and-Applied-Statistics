ee8f591f797badde055ad882d6adae7b7970af96
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:50:13 -0400

    Add documentation comments for FightingEnergy class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/FightingEnergy.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/FightingEnergy.java
@@ -2,8 +2,18 @@ package card.energy.energycards;
 
 import card.energy.EnergyCard;
 
+/**
+ * This class represents a "Fighting Energy" card in the Pokémon Trading Card Game.
+ * It extends the EnergyCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class FightingEnergy extends EnergyCard
 {
+    /**
+     * Constructor to initialize a "Fighting Energy" card with its type and value.
+     */
     public FightingEnergy()
     {
         super("Fighting Energy", "Fighting", 1);
