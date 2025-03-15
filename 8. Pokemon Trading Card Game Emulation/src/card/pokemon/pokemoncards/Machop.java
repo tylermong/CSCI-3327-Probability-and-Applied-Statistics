8d4869958da4d276eb0595a2e258889efc14495b
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:35:49 -0400

    Add documentation comments for Machop class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Machop.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Machop.java
@@ -5,8 +5,20 @@ import java.util.Map;
 import card.pokemon.PokemonCard;
 import card.pokemon.helper.Move;
 
+/**
+ * This class represents a "Machop" Pokémon card in the Pokémon Trading Card Game.
+ * It extends the PokemonCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * It also adds a unique move to the card: "Low Kick".
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Machop extends PokemonCard
 {
+    /**
+     * Constructor to initialize a "Machop" Pokémon card with its unique attributes.
+     * It also adds a unique move to the card: "Low Kick".
+     */
     public Machop()
     {
         super("Machop", 50, "Psychic", null, 1);
