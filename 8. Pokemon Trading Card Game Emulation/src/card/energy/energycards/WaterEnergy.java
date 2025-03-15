30ea53d1443bd3000ab6063751d1b007ff5af805
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:49:56 -0400

    Add documentation comments for WaterEnergy class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/WaterEnergy.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/WaterEnergy.java
@@ -2,8 +2,18 @@ package card.energy.energycards;
 
 import card.energy.EnergyCard;
 
+/**
+ * This class represents a "Water Energy" card in the Pokémon Trading Card Game.
+ * It extends the EnergyCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class WaterEnergy extends EnergyCard
 {
+    /**
+     * Constructor to initialize a "Water Energy" card with its type and value.
+     */
     public WaterEnergy()
     {
         super("Water Energy", "Water", 1);
