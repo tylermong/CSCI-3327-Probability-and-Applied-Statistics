d095e86b77c4625535d8590b39aa685865756bb2
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 15:48:51 -0400

    Add documentation comments to Card class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/Card.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/Card.java
@@ -1,21 +1,42 @@
 package card;
 
+/**
+ * This class represents a generic card in the Pokémon Trading Card Game.
+ * It provides methods to get the card name and card type.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Card
 {
+    // Variables to store the name and type of the card.
     private String name;
     private String type;
 
+    /**
+     * Constructor to initialize a card with a name and type.
+     * @param name  The name of the card.
+     * @param type  The type of the card (e.g., Pokémon, Energy, Trainer).
+     */
     public Card(String name, String type)
     {
         this.name = name;
         this.type = type;
     }
 
+    /**
+     * Returns the name of the card.
+     * @return  The name of the card.
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * Returns the type of the card.
+     * @return  The type of the card (e.g., Pokémon, Energy, Trainer).
+     */
     public String getType()
     {
         return type;
