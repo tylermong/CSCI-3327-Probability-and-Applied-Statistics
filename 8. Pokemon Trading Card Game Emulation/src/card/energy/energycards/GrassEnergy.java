d154b04fc4c7287d62d69c7d00dfa61a6e8d0062
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:50:06 -0400

    Add documentation comments for GrassEnergy class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/GrassEnergy.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/GrassEnergy.java
@@ -2,8 +2,18 @@ package card.energy.energycards;
 
 import card.energy.EnergyCard;
 
+/**
+ * This class represents a "Grass Energy" card in the Pokémon Trading Card Game.
+ * It extends the EnergyCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class GrassEnergy extends EnergyCard
 {
+    /**
+     * Constructor to initialize a "Grass Energy" card with its type and value.
+     */
     public GrassEnergy()
     {
         super("Grass Energy", "Grass", 1);
