6a85769f6443a10582aa397be031d662eb25ff8b
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:36:02 -0400

    Add documentation comments for Seel class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Seel.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Seel.java
@@ -5,8 +5,20 @@ import java.util.Map;
 import card.pokemon.PokemonCard;
 import card.pokemon.helper.Move;
 
+/**
+ * This class represents a "Seel" Pokémon card in the Pokémon Trading Card Game.
+ * It extends the PokemonCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * It also adds a unique move to the card: "Headbutt".
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Seel extends PokemonCard
 {
+    /**
+     * Constructor to initialize a "Seel" Pokémon card with its unique attributes.
+     * It also adds a unique move to the card: "Headbutt".
+     */
     public Seel()
     {
         super("Seel", 50, "Lightning", null, 1);
