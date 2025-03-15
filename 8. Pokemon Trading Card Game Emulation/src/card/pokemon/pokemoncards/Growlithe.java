2fd93f9894fc39888d0741a860e90e699ff56abd
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:35:44 -0400

    Add documentation comments for Growlithe class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Growlithe.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Growlithe.java
@@ -5,8 +5,20 @@ import java.util.Map;
 import card.pokemon.PokemonCard;
 import card.pokemon.helper.Move;
 
+/**
+ * This class represents a "Growlithe" Pokémon card in the Pokémon Trading Card Game.
+ * It extends the PokemonCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * It also adds a unique move to the card: "Flare".
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Growlithe extends PokemonCard
 {
+    /**
+     * Constructor to initialize a "Growlithe" Pokémon card with its unique attributes.
+     * It also adds a unique move to the card: "Flare".
+     */
     public Growlithe()
     {
         super("Growlithe", 60, "Water", null, 1);
