c0c6470c2e4003a55e7e7bc6c1ea185fb29a8d75
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 15:57:20 -0400

    Add documentation comments to Bill class and its methods

--- 8. Pokemon Trading Card Game Emulation/src/card/trainer/trainercards/Bill.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/trainer/trainercards/Bill.java
@@ -6,13 +6,29 @@ import pile.piles.Deck;
 import pile.piles.DiscardPile;
 import pile.piles.Hand;
 
+/**
+ * This class represents the Trainer card "Bill" in the Pokémon Trading Card Game.
+ * It extends the TrainerCard class and provides an overridden method to use the unique effect of the card.
+ * The effect of this card is to draw 2 cards from the player's deck.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Bill extends TrainerCard
 {
+    /**
+     * Constructor to initialize the "Bill" trainer card with its name and description.
+     * This constructor calls the superclass constructor to set the name and description of the card.
+     */
     public Bill()
     {
         super("Bill", "Draw 2 cards.");
     }
 
+    /**
+     * Uses the effect of the "Bill" trainer card, which is to draw 2 cards from the player's deck.
+     * Prints a message indicating the effect being used and calls the TrainerEffect class to perform the action.
+     */
     @Override
     public void useEffect(Deck deck, Hand hand, DiscardPile discardPile)
     {
