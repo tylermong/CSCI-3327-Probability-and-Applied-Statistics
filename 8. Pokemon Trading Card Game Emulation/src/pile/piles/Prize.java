fdf77ec7174c3c645622866967c53cd8ae715cce
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 15:43:24 -0400

    Add documentation comments to Prize class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/pile/piles/Prize.java
+++ 8. Pokemon Trading Card Game Emulation/src/pile/piles/Prize.java
@@ -2,8 +2,18 @@ package pile.piles;
 
 import pile.Pile;
 
+/**
+ * Represents the prize pile in the Pokémon Trading Card Game, where the player's prize cards are placed.
+ * This class extends the Pile class and is used to manage the player's prize cards.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Prize extends Pile
 {
+    /**
+     * Constructor for the Prize class. Initializes the prize pile for the player.
+     */
     public Prize()
     {
         super();
