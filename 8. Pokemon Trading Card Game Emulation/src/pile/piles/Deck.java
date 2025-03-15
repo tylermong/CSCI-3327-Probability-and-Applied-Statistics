3d95f9c1d42a45a21842879f9565e2c99850e8df
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 15:22:57 -0400

    Add documentation comments to Deck class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/pile/piles/Deck.java
+++ 8. Pokemon Trading Card Game Emulation/src/pile/piles/Deck.java
@@ -2,8 +2,18 @@ package pile.piles;
 
 import pile.Pile;
 
+/**
+ * Represents the deck pile in the Pokémon Trading Card Game.
+ * This class extends the Pile class and is used to manage the player's deck of cards.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Deck extends Pile
 {
+    /**
+     * Constructor for the Deck class. Initializes the deck pile for the player.
+     */
     public Deck()
     {
         super();
