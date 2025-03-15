3bd87ad1431fcd5d5313084664bcd546faccf11d
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:35:37 -0400

    Add documentation comments for Diglett class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Diglett.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Diglett.java
@@ -5,8 +5,20 @@ import java.util.Map;
 import card.pokemon.PokemonCard;
 import card.pokemon.helper.Move;
 
+/**
+ * This class represents a "Diglett" Pokémon card in the Pokémon Trading Card Game.
+ * It extends the PokemonCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * It also adds two unique moves to the card: "Dig" and "Mud Slap".
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Diglett extends PokemonCard
 {
+    /**
+     * Constructor to initialize a "Diglett" Pokémon card with its unique attributes.
+     * It also adds two unique moves to the card: "Dig" and "Mud Slap".
+     */
     public Diglett()
     {
         super("Diglett", 30, "Grass", "Lightning", 0);
