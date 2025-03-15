6a8aa1bfa70bba6d503c2f0fa2ed308f263cfdca
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:35:47 -0400

    Add documentation comments for Hitmonchan class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Hitmonchan.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/Hitmonchan.java
@@ -5,8 +5,20 @@ import java.util.Map;
 import card.pokemon.PokemonCard;
 import card.pokemon.helper.Move;
 
+/**
+ * This class represents a "Hitmonchan" Pokémon card in the Pokémon Trading Card Game.
+ * It extends the PokemonCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * It also adds two unique moves to the card: "Jab" and "Special Punch".
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class Hitmonchan extends PokemonCard
 {
+    /**
+     * Constructor to initialize a "Hitmonchan" Pokémon card with its unique attributes.
+     * It also adds two unique moves to the card: "Jab" and "Special Punch".
+     */
     public Hitmonchan()
     {
         super("Hitmonchan", 70, "Psychic", null, 2);
