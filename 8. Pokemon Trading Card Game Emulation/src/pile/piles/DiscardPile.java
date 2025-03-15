bf00bee0b232861cae3a504d888b5ca230043561
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 15:24:45 -0400

    Add documentation comments to DiscardPile class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/pile/piles/DiscardPile.java
+++ 8. Pokemon Trading Card Game Emulation/src/pile/piles/DiscardPile.java
@@ -2,8 +2,18 @@ package pile.piles;
 
 import pile.Pile;
 
+/**
+ * Represents the discard pile in the Pokémon Trading Card Game, where the player's discarded cards are placed.
+ * This class extends the Pile class and is used to manage the player's discarded cards.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class DiscardPile extends Pile
 {
+    /**
+     * Constructor for the DiscardPile class. Initializes the discard pile for the player.
+     */
     public DiscardPile()
     {
         super();
