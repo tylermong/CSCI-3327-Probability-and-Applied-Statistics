49203d616ae3ce81183537345597c963259e8811
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-15 16:50:00 -0400

    Add documentation comments for PsychicEnergy class and its constructor

--- 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/PsychicEnergy.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/PsychicEnergy.java
@@ -2,8 +2,18 @@ package card.energy.energycards;
 
 import card.energy.EnergyCard;
 
+/**
+ * This class represents a "Psychic Energy" card in the Pokémon Trading Card Game.
+ * It extends the EnergyCard class, calls the superclass constructor to initialize the card with its unique attributes.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
 public class PsychicEnergy extends EnergyCard
 {
+    /**
+     * Constructor to initialize a "Psychic Energy" card with its type and value.
+     */
     public PsychicEnergy()
     {
         super("Psychic Energy", "Psychic", 1);
