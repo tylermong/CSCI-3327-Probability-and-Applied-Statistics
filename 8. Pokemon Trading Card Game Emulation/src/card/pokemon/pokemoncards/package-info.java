293c4a8f0413985add42eca4b7bb6b5aa0e5a28d
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 01:36:55 -0400

    Add author and version information to package-info.java for card.pokemon.pokemoncards package

--- 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/package-info.java
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/package-info.java
@@ -2,6 +2,9 @@
  * This pacakge contains all the Pokémon cards in the game.
  * Pokémon cards are the main cards used in the game to battle against each other.
  * Each Pokémon card has a name, set of attributes, and a set of moves that can be used in battle.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
  */
 
 package card.pokemon.pokemoncards;
