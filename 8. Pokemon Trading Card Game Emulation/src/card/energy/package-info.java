e2ea6e3586c2b1d9a18c4fd4c65646cb68404c20
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 01:24:15 -0400

    Add card.energy package documentation

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains classes relatd to Energy cards in the Pokémon Trading Card Game (TCG) emulation.
+ * It includes the EnergyCard class, which represents an Energy card and its attributes such as type and value.
+ * The package is designed to manage the functionality and interactions of Energy cards within the game,
+ * ensuring that they can be used properly in gameplay for Pokémon moves and retreats.
+ * 
+ * Sub-packages include:
+ * - {@link card.energy.energycards}: For specific Energy cards like Fire, Water, and Double Colorless Energy.
+ */
+
+package card.energy;
