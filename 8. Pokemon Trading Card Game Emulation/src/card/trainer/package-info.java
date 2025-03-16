4f3499b4e3163e07c3103c6fd2c36904a7603b1d
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 01:39:37 -0400

    Add author and version information to package-info.java for card.trainer package

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/trainer/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * This package contains classes related to Trainer cards in the Pokémon Trading Card Game (TCG) emulation.
+ * It includes the TrainerCard class, which represents a Trainer card and its attributes such as name, type, and effects.
+ * The package is designed to manage the functionality and interactions of Trainer cards within the game,
+ * ensuring that they can be used properly in gameplay for drawing and discarding cards.
+ * 
+ * Sub-packages include:
+ * - {@link card.trainer.trainercards}: For specific Trainer cards like Bill and Professor Oak.
+ * - {@link card.trainer.helper}: For a helper class that assists in performing actions related to Trainer cards.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
+
+package card.trainer;
