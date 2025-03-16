0be72f34479df034cc5ece8cd1d8ce76ab4c8f76
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 01:26:49 -0400

    Add package-info.java for card.energy.energycards package

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/energy/energycards/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains all the energy cards in the game.
+ * Energy cards are used to allow Pokémon to use their moves as well as to pay for retreat costs.
+ * Each energy card has a specific type and value associated with it, which determines its use in the game.
+ */
+
+package card.energy.energycards;
