2a81cac57c4f341d03dbf836113c862730c7adfe
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 01:31:18 -0400

    Add package-info.java for card.pokemon package

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package contains the classes related to Pokémon cards in the Pokémon Trading Card Game (TCG) emulation.
+ * It includes the PokemonCard class, which represents a Pokémon card and its attributes such as name, HP, weakness, resistance, and moves.
+ * The package is designed to manage the functionality and interactions of Pokémon cards within the game,
+ * ensuring that they can be used properly in gameplay for attacking and other actions.
+ * 
+ * Sub-packages include:
+ * - {@link card.pokemon.pokemoncards}: For specific Pokémon cards like Diglett, Machop, Staryu, and others.
+ * - {@link card.pokemon.helper}: For the move helper class that assists in performing actions related to Pokémon moves.
+ */
+
+package card.pokemon;
