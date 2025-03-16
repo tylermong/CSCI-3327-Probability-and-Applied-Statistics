a2d9f965db6f3106addd80b1ad3d1b077633c916
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 01:41:00 -0400

    Add package-info.java for card.trainer.trainercards package with description, author, and version information

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/trainer/trainercards/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains all the trainer cards in the game.
+ * Trainer cards are used to perform actions such as drawing and discarding cards.
+ * Each trainer card has a name and effect that can be used in the game to perform actions.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
+
+package card.trainer.trainercards;
