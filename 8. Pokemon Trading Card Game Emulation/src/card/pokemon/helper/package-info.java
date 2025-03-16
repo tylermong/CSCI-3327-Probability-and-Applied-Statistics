04f41d13be79a2202e34b27fd3f0f5fc3562ca91
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 01:35:42 -0400

    Add package-info.java for card.pokemon.helper package

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/helper/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the helper class for the Pokémon's moves.
+ * The helper class is used to easily access the move name, damage, energy cost, and effect of a move.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
+
+package card.pokemon.helper;
