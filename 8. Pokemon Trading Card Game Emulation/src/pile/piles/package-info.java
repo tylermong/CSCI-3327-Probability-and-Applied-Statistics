f7f8a139e96666cf0c527675e084a158081f7f23
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 01:47:10 -0400

    Add package-info.java for pile.piles package with description, author, and version information

---
+++ 8. Pokemon Trading Card Game Emulation/src/pile/piles/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the classes for the different piles in the game.
+ * Piles are used to store the cards in the game, such as the deck, discard pile, active pile, and prize pile.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
+
+package pile.piles;
