92ad1c535c0a09c5d6f4a3547cc23d35a978d127
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 01:41:59 -0400

    Add package-info.java for card.trainer.helper package with description, author, and version information

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/trainer/helper/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the helper class for the trainer cards.
+ * The helper class is used to easily access the draw and discard methods for the trainer cards.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
+
+package card.trainer.helper;
