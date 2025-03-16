ea8baf7eeccf2b17a1a8e3be7419e8b9a8273ddf
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 01:33:31 -0400

    Add package-info.java for card.pokemon.pokemoncards package

---
+++ 8. Pokemon Trading Card Game Emulation/src/card/pokemon/pokemoncards/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This pacakge contains all the Pokémon cards in the game.
+ * Pokémon cards are the main cards used in the game to battle against each other.
+ * Each Pokémon card has a name, set of attributes, and a set of moves that can be used in battle.
+ */
+
+package card.pokemon.pokemoncards;
