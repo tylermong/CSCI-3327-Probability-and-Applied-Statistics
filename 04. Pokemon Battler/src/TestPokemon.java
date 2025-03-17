c8c7387a4b4647caeab8beb00cc2be2c7f132fbd
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 20:18:05 -0400

    Add comments to TestPokemon class to clarify battle testing

--- 04. Pokemon Battler/src/TestPokemon.java
+++ 04. Pokemon Battler/src/TestPokemon.java
@@ -2,9 +2,11 @@ public class TestPokemon
 {
     public static void main(String[] args)
     {
+        // Create two Pokemon objects to test the battle method
         Charmander charmander1 = new Charmander();
         Pikachu pikachu1 = new Pikachu();
 
+        // Battle the two Pokemon
         Stadium tester = new Stadium();
         tester.battle(charmander1, pikachu1);
     }
