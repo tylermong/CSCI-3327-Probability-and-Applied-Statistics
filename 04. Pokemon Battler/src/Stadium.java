511af059295b8ca5ec64dce8b2bfdaa88939ab9f
tylermong <117101211+tylermong@users.noreply.github.com>
2025-03-16 20:17:24 -0400

    Add comments to clarify battle logic in Stadium class

--- 04. Pokemon Battler/src/Stadium.java
+++ 04. Pokemon Battler/src/Stadium.java
@@ -2,6 +2,7 @@ public class Stadium
 {
     public void battle(Pokemon p1, Pokemon p2)
     {
+        // Whoever has the higher speed should attack first
         if (p1.getSpeed() > p2.getSpeed())
         {
             while (p1.getHp() > 0 && p2.getHp() > 0)
@@ -19,7 +20,8 @@ public class Stadium
             }
         }
 
-        if (p1.getHp() <= 0)
+        // After the battle loop is over, print the winner based on who has <= 0 HP
+        if (p2.getHp() <= 0)
         {
             System.out.println("Player 1 wins!");
         }
