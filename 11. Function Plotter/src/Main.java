0c6befa2024e2279b297c7f3c925ec8763e35bc0
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-03-27 18:25:16 -0400

    Add initial implementation of Function Plotter with data generation and salting features

---
+++ 11. Function Plotter/src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        DataBuilder dataBuilder = new DataBuilder();
+        dataBuilder.run();
+
+        Salter salter = new Salter();
+        salter.run();
+    }    
+}
