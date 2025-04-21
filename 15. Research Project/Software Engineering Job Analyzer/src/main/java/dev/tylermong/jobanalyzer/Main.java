f5b0270c0ef2f24b9b7ce3d83399b8c09f1e452f
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-04-21 12:44:28 -0400

    add workday scraper implementation

--- 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/Main.java
+++ 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/Main.java
@@ -1,22 +1,59 @@
 package dev.tylermong.jobanalyzer;
 
 import dev.tylermong.jobanalyzer.scraper.SimplifyInternshipScraper;
+import dev.tylermong.jobanalyzer.scraper.WorkdayScraper;
+import dev.tylermong.jobanalyzer.model.JobPost;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        SimplifyInternshipScraper scraper = new SimplifyInternshipScraper();
-        
+        SimplifyInternshipScraper simplifyScraper = new SimplifyInternshipScraper();
+        List<String> links = new ArrayList<>();
         try
         {
+            links = simplifyScraper.scrapeLinks();
+        }
+        catch (IOException e)
+        {
+            System.err.println("Error scraping links: " + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+
+        WorkdayScraper workdayScraper = new WorkdayScraper();
+
+        int count = 0;
+        for (String link : links)
+        {
+            if (link.contains("workday") && count < 1)
+            {
+                try
+                {
+                    JobPost jobPost = workdayScraper.scrapeJob(link);
+                    System.out.println("Company: " + jobPost.getCompany());
+                    System.out.println("Title: " + jobPost.getTitle());
+                    System.out.println("Location: " + jobPost.getLocation());
+                    System.out.println("Skills: " + jobPost.getSkills());
+                    System.out.println("URL" + jobPost.getUrl());
+                    count++;
+                }
+                catch (IOException e)
+                {
+                    System.err.println("Error scraping job from link " + link + ": " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        /*try
+        {
             List<String> jobPostingURLs = scraper.scrapeLinks();
-            
+
             try (BufferedWriter writer = new BufferedWriter(new FileWriter("results.txt")))
             {
                 for (String url : jobPostingURLs)
@@ -35,6 +72,6 @@ public class Main
         catch (Exception e)
         {
             e.printStackTrace();
-        }
+        }*/
     }
 }
