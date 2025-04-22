09b71ba841649c02a5503a92563540493bf42b2a
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-04-22 12:06:54 -0400

    refactor: move job scraping into own file

---
+++ 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/JobScrapingService.java
@@ -0,0 +1,75 @@
+package dev.tylermong.jobanalyzer;
+
+import dev.tylermong.jobanalyzer.scraper.SimplifyInternshipScraper;
+import dev.tylermong.jobanalyzer.scraper.WorkdayScraper;
+import dev.tylermong.jobanalyzer.model.JobPost;
+import dev.tylermong.jobanalyzer.util.ProgressBar;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class JobScrapingService
+{
+    private final SimplifyInternshipScraper linkScraper;
+    private final WorkdayScraper jobScraper;
+    private final String outputFile;
+
+    public JobScrapingService(
+            SimplifyInternshipScraper linkScraper,
+            WorkdayScraper jobScraper,
+            String outputFile)
+    {
+        this.linkScraper = linkScraper;
+        this.jobScraper = jobScraper;
+        this.outputFile = outputFile;
+    }
+
+    public void scrapeAndWrite() throws IOException
+    {
+        List<String> links = linkScraper.scrapeLinks();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile)))
+        {
+            ProgressBar bar = new ProgressBar(50);
+            int total = links.size();
+            int processed = 0;
+            System.out.println("Scraping job posts from " + total + " links");
+            for (String link : links)
+            {
+                processed++;
+                bar.update(processed, total);
+                if (link.contains("workday"))
+                {
+                    try
+                    {
+                        JobPost post = jobScraper.scrapeJob(link);
+                        writePost(writer, post);
+                    }
+                    catch (IOException e)
+                    {
+                        System.err.println("\nError scraping " + link + ": " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+            }
+            System.out.println("\nDone. Results in " + outputFile);
+        }
+    }
+
+    private void writePost(BufferedWriter w, JobPost p) throws IOException
+    {
+        w.write("Company: " + p.getCompany());
+        w.newLine();
+        w.write("Title: " + p.getTitle());
+        w.newLine();
+        w.write("Location: " + p.getLocation());
+        w.newLine();
+        w.write("Skills: " + p.getSkills());
+        w.newLine();
+        w.write("URL: " + p.getUrl());
+        w.newLine();
+        w.newLine();
+        w.flush();
+    }
+}
