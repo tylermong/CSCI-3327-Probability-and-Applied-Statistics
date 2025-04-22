1a32a3b4190e2436cb944877ded127e4bd6ecbfc
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-04-22 12:06:40 -0400

    refactor: move progress bar into own class

---
+++ 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/util/ProgressBar.java
@@ -0,0 +1,17 @@
+package dev.tylermong.jobanalyzer.util;
+
+public class ProgressBar {
+    private final int width;
+    public ProgressBar(int width) {
+        this.width = width;
+    }
+    public void update(int processed, int total) {
+        double pct = (double) processed / total;
+        int done = (int)(width * pct), left = width - done;
+        StringBuilder b = new StringBuilder("\r");
+        for (int i = 0; i < done; i++) b.append('█');
+        for (int i = 0; i < left; i++) b.append('▒');
+        b.append(String.format(" %.1f%% (%d/%d)", pct * 100, processed, total));
+        System.out.print(b);
+    }
+}
