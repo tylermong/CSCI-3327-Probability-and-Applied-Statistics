7608c967ce541367755cef7d4f68c171129c0537
tylermong <117101211+tylermong@users.noreply.github.com>
2025-04-26 14:34:15 -0400

    feat: add link scrapers to ScraperFactory

--- 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/scraper/ScraperFactory.java
+++ 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/scraper/ScraperFactory.java
@@ -1,11 +1,24 @@
 package dev.tylermong.jobanalyzer.scraper;
 import dev.tylermong.jobanalyzer.scraper.data.*;
+import dev.tylermong.jobanalyzer.scraper.postings.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ScraperFactory
 {
+    public static List<LinkScraper> createLinkScrapers()
+    {
+        List<LinkScraper> linkScrapers = new ArrayList<>();
+
+        linkScrapers.add(new SimplifyInternshipScraper());
+        linkScrapers.add(new VanshInternshipScraper());
+
+        return linkScrapers;
+    }
+
     public static Map<String, DataScraper> createDataScrapers()
     {
         Map<String, DataScraper> dataScrapers = new HashMap<>();
