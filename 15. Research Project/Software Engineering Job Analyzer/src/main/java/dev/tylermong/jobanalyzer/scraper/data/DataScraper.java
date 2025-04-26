4a2446ca25939d83af3cf6b00fd4d8a4ce79abdf
tylermong <117101211+tylermong@users.noreply.github.com>
2025-04-26 14:17:50 -0400

    refactor: rename JobScaper to DataScraper for clarity

--- 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/scraper/data/DataScraper.java
+++ 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/scraper/data/DataScraper.java
@@ -3,7 +3,7 @@ package dev.tylermong.jobanalyzer.scraper.data;
 import dev.tylermong.jobanalyzer.model.JobPost;
 import java.io.IOException;
 
-public interface JobScraper
+public interface DataScraper
 {
     JobPost scrapeJob(String link) throws IOException;
 }
