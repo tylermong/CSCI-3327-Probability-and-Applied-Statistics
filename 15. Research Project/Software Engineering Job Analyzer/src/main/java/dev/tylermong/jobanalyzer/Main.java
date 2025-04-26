4eabf38fe603682a129b0f687dca0d6e823150aa
tylermong <117101211+tylermong@users.noreply.github.com>
2025-04-26 14:34:33 -0400

    refactor: replace direct scraper instantiation with ScraperFactory in Main class

--- 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/Main.java
+++ 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/Main.java
@@ -1,8 +1,6 @@
 package dev.tylermong.jobanalyzer;
 
 import dev.tylermong.jobanalyzer.scraper.ScraperFactory;
-import dev.tylermong.jobanalyzer.scraper.postings.SimplifyInternshipScraper;
-import dev.tylermong.jobanalyzer.scraper.postings.VanshInternshipScraper;
 import dev.tylermong.jobanalyzer.analyzer.SkillAnalyzer;
 
 import java.io.IOException;
@@ -12,8 +10,7 @@ public class Main
     public static void main(String[] args) throws IOException
     {
         new JobScrapingService(
-                new SimplifyInternshipScraper(),
-                new VanshInternshipScraper(),
+                ScraperFactory.createLinkScrapers(),
                 ScraperFactory.createDataScrapers(),
                 "15. Research Project/Software Engineering Job Analyzer/output/DataResults.txt").scrapeAndWrite();
 
