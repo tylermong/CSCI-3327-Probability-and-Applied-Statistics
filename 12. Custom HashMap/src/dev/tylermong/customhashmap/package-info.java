88a0f74241cfa7e24c96a031a09b99c01203a150
tylermong <117101211+tylermong@users.noreply.github.com>
2025-04-27 20:38:56 -0400

    docs: add package-info.java with project overview and class descriptions

---
+++ 12. Custom HashMap/src/dev/tylermong/customhashmap/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains all the classes for the CustomHashMap project. It includes the {@code CustomHashMap} class,
+ * which implements a custom hash map data structure, the {@code Entry} class, which represents a key-value pair in the
+ * hash map, the {@code CustomHashMapTester} class, which contains unit and performance tests for the
+ * {@code CustomHashMap} class, and the {@code Main} class, which serves as the entry point for the application.
+ * 
+ * @author  Tyler Mong
+ * @version 1.0
+ */
+
+package dev.tylermong.customhashmap;
