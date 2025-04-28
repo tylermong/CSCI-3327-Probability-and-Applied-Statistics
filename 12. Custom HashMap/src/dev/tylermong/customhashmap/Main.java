2823a4d022b1d9e4af08b370d2dd792acca0ad5b
tylermong <117101211+tylermong@users.noreply.github.com>
2025-04-27 20:07:21 -0400

    docs: enhance documentation for Main class with detailed method descriptions

--- 12. Custom HashMap/src/dev/tylermong/customhashmap/Main.java
+++ 12. Custom HashMap/src/dev/tylermong/customhashmap/Main.java
@@ -1,7 +1,16 @@
 package dev.tylermong.customhashmap;
 
+/**
+ * This class serves as the entry point for the application. It creates an instance of the CustomHashMapTester class and
+ * runs all tests.
+ */
 public class Main
 {
+    /**
+     * The main method that runs the application. It creates an instance of CustomHashMapTester and runs all tests.
+     * 
+     * @param args command line arguments
+     */
     public static void main(String[] args)
     {
         CustomHashMapTester customHashMapTester = new CustomHashMapTester();
