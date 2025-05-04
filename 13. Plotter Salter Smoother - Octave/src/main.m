266673fad1953216460f8a034aa4f8f27f6b73e6
tylermong <117101211+tylermong@users.noreply.github.com>
2025-05-04 18:58:10 -0400

    feat: add message indicating x, y, salted y, and smoothed y values

--- 13. Plotter Salter Smoother - Octave/src/main.m
+++ 13. Plotter Salter Smoother - Octave/src/main.m
@@ -11,3 +11,7 @@ smoothedYValues = smoother(xValues, saltedYValues);
 
 % Plot all datasets together for comparison
 plotalldata(xValues, yValues, saltedYValues, smoothedYValues);
+printf('X values: %s\n', mat2str(xValues));
+printf('Y values: %s\n', mat2str(yValues));
+printf('Salted Y values: %s\n', mat2str(saltedYValues));
+printf('Smoothed Y values: %s\n', mat2str(smoothedYValues));
