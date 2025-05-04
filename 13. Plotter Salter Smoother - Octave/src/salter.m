bc394e9da08a0ac7610dfe50dd9df115bf509d59
tylermong <117101211+tylermong@users.noreply.github.com>
2025-05-04 18:44:53 -0400

    refactor: replace fprintf with printf

--- 13. Plotter Salter Smoother - Octave/src/salter.m
+++ 13. Plotter Salter Smoother - Octave/src/salter.m
@@ -1,9 +1,9 @@
 function saltedYValues = salter(xValues, yValues)
     variability = 30;
-    fprintf('\nVariability = 30: ');
+    printf('\nVariability = 30: ');
     
     % Salts the y values by adding random noise based on the variability
     saltedYValues = yValues + (rand(size(yValues)) * 2 - 1) * variability;
         
-    fprintf('Salting complete\n');
+    printf('Salting complete\n');
 end
