c4bb48f928175b79d0c6a49c51b85d6c501c4c52
Tyler Mong <117101211+tylermong@users.noreply.github.com>
2025-05-03 22:59:21 -0400

    feat: implement smoother function for moving average of salted y values

---
+++ 13. Plotter Salter Smoother - Octave/smoother.m
@@ -0,0 +1,16 @@
+function smoothedYValues = smoother(xValues, saltedYValues)
+    windowValue = 2;
+    fprintf('\nWindow value = 2\n');
+
+    % Initialize the smoothed y values array
+    smoothedYValues = zeros(size(saltedYValues));
+
+    % Smooth the salted y values using a moving average based on the window value
+    for i = 1:length(saltedYValues)
+        leftBound = max(1, i - windowValue);
+        rightBound = min(length(saltedYValues), i + windowValue);
+
+        window = saltedYValues(leftBound:rightBound);
+        smoothedYValues(i) = mean(window);
+    end
+end
