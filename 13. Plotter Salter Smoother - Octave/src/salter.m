52eaa2acd919a390363cd25b6ec67d1c0b68ef75
tylermong <117101211+tylermong@users.noreply.github.com>
2025-05-04 18:57:44 -0400

    fix: remove unnecessary colon

--- 13. Plotter Salter Smoother - Octave/src/salter.m
+++ 13. Plotter Salter Smoother - Octave/src/salter.m
@@ -1,6 +1,6 @@
 function saltedYValues = salter(xValues, yValues)
     variability = 30;
-    printf('\nVariability = 30: ');
+    printf('\nVariability = 30\n');
     
     % Salts the y values by adding random noise based on the variability
     saltedYValues = yValues + (rand(size(yValues)) * 2 - 1) * variability;
