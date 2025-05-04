e96eace4e144dd27bb9c88e2d4fbccac444327ff
tylermong <117101211+tylermong@users.noreply.github.com>
2025-05-04 18:47:29 -0400

    feat: add output notifying using default x values

--- 13. Plotter Salter Smoother - Octave/src/dataBuilder.m
+++ 13. Plotter Salter Smoother - Octave/src/dataBuilder.m
@@ -3,11 +3,12 @@ function [xValues, yValues] = dataBuilder()
     printf('Function to plot: f(x) = x^2\n');
 
     printf('\nSpecify x values using brackets (e.g. [1, 2, 3, 4])\n');
-    printf('Or press Enter to use the default range (1 to 10): ');
+    printf('or press Enter to use the default range (1 to 10): ');
     xValues = input('');
 
     if isempty(xValues)
         xValues = 1:10;
+        printf('Using default x values')
     end
 
     yValues = power(xValues, 2);
