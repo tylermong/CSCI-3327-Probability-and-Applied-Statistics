6daef2a52583722918ed9c36927f204fc19b2f1b
tylermong <117101211+tylermong@users.noreply.github.com>
2025-05-06 00:12:46 -0400

    style: format ProgressBar class for improved readability

--- 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/util/ProgressBar.java
+++ 15. Research Project/Software Engineering Job Analyzer/src/main/java/dev/tylermong/jobanalyzer/util/ProgressBar.java
@@ -1,17 +1,30 @@
 package dev.tylermong.jobanalyzer.util;
 
-public class ProgressBar {
+public class ProgressBar
+{
     private final int width;
-    public ProgressBar(int width) {
+
+    public ProgressBar(int width)
+    {
         this.width = width;
     }
-    public void update(int processed, int total) {
-        double pct = (double) processed / total;
-        int done = (int)(width * pct), left = width - done;
+
+    public void update(int processed, int total)
+    {
+        double percent = (double) processed / total;
+        int done = (int) (width * percent), left = width - done;
         StringBuilder b = new StringBuilder("\r");
-        for (int i = 0; i < done; i++) b.append('█');
-        for (int i = 0; i < left; i++) b.append('▒');
-        b.append(String.format(" %.1f%% (%d/%d)", pct * 100, processed, total));
+        
+        for (int i = 0; i < done; i++)
+        {
+            b.append('█');
+        }
+        for (int i = 0; i < left; i++)
+        {
+            b.append('▒');
+        }
+        b.append(String.format(" %.1f%% (%d/%d)", percent * 100, processed, total));
+        
         System.out.print(b);
     }
 }
