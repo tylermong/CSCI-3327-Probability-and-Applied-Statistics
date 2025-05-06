b9ef065a47d0cba47696dd37d76a6de4b1e17896
tylermong <117101211+tylermong@users.noreply.github.com>
2025-05-06 02:53:57 -0400

    refactor: remove unused imports in APIPlotData class

--- 14. Plotter Salter Smoother - API/src/main/java/dev/tylermong/plottersaltersmootherapi/APIPlotData.java
+++ 14. Plotter Salter Smoother - API/src/main/java/dev/tylermong/plottersaltersmootherapi/APIPlotData.java
@@ -4,8 +4,6 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
